ffe939aa50c0252946df506b38fa98253ae9528d
Christopher Krohn <me@christopher-krohn.de>
2026-06-03 13:38:22 +0200

    Updates

--- src/music/pitch.ts
+++
@@ -1,7 +0,0 @@
-export const midiToFrequency = (midi: number): number => {
-  if (!Number.isFinite(midi)) {
-    throw new Error(`MIDI value must be a finite number. Received: ${midi}`);
-  }
-
-  return 440 * 2 ** ((midi - 69) / 12);
-};
--- src/state/sessionStore.ts
+++
@@ -1,7 +0,0 @@
-export type SessionState = {
-  questionCount: number;
-};
-
-export const INITIAL_SESSION_STATE: SessionState = {
-  questionCount: 0
-};
