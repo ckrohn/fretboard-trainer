943f3f1417ef957291df63ff83e9bab349b62c71
Christopher Krohn <me@christopher-krohn.de>
2026-06-03 13:42:53 +0200

    Update deployment scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()]
-});
+export default defineConfig(({ command, mode }) => {
+  const isGitHubPages = mode === 'github-pages'
+
+  return {
+    plugins: [react()],
+    base: isGitHubPages ? '/fretboard-trainer/' : '/',
+  }
+})
