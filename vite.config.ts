64e87bc3089066d5fb20e36765ba5f1da6dac92f
Christopher Krohn <me@christopher-krohn.de>
2026-06-03 13:44:27 +0200

    Revert "Update deployment scripts"
    This reverts commit 943f3f1417ef957291df63ff83e9bab349b62c71.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command, mode }) => {
-  const isGitHubPages = mode === 'github-pages'
-
-  return {
-    plugins: [react()],
-    base: isGitHubPages ? '/fretboard-trainer/' : '/',
-  }
-})
+export default defineConfig({
+  plugins: [react()]
+});
